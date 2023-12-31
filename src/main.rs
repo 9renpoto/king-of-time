9780dfa815da4c1ee618e4c2aaa83982a0175e23
Keisuke Umeno <9renpoto@gmail.com>
2023-12-31 18:03:59 +0900

    fix: remove tracing

--- src/main.rs
+++ src/main.rs
@@ -41,14 +41,6 @@ async fn handler(event: LambdaEvent<Request>) -> Result<Response, Error> {
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    tracing_subscriber::fmt()
-        .with_max_level(tracing::Level::INFO)
-        // disable printing the name of the module in every log line.
-        .with_target(false)
-        // disabling time is handy because CloudWatch will add the ingestion time.
-        .without_time()
-        .init();
-
     run(service_fn(handler)).await
 }
 
