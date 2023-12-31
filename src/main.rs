9d770bcb12133738999a0b9467b08b4c603b74f3
Keisuke Umeno <9renpoto@gmail.com>
2023-12-31 16:23:06 +0900

    feat: add lambda sample

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,63 @@
-fn main() {
-    println!("Hello, world!");
+use lambda_runtime::{run, service_fn, Error, LambdaEvent};
+
+use serde::{Deserialize, Serialize};
+
+/// This is a made-up example. Requests come into the runtime as unicode
+/// strings in json format, which can map to any structure that implements `serde::Deserialize`
+/// The runtime pays no attention to the contents of the request payload.
+#[derive(Deserialize)]
+struct Request {
+    command: String,
 }
 
-pub fn prints_and_returns_10(a: i32) -> i32 {
-    //{}という値を得た
-    println!("I got the value {}", a);
-    10
+/// This is a made-up example of what a response structure may look like.
+/// There is no restriction on what it can be. The runtime requires responses
+/// to be serialized into json. The runtime pays no attention
+/// to the contents of the response payload.
+#[derive(Serialize)]
+struct Response {
+    req_id: String,
+    msg: String,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+/// This is the main body for the function.
+/// Write your code inside it.
+/// There are some code example in the following URLs:
+/// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
+/// - https://github.com/aws-samples/serverless-rust-demo/
+async fn handler(event: LambdaEvent<Request>) -> Result<Response, Error> {
+    // Extract some useful info from the request
+    let command = event.payload.command;
 
-    #[test]
-    fn this_test_will_pass() {
-        let value = prints_and_returns_10(4);
-        assert_eq!(10, value);
-    }
+    // Prepare the response
+    let resp = Response {
+        req_id: event.context.request_id,
+        msg: format!("Command {}.", command),
+    };
+
+    // Return `Response` (it will be serialized to JSON automatically by the runtime)
+    Ok(resp)
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::INFO)
+        // disable printing the name of the module in every log line.
+        .with_target(false)
+        // disabling time is handy because CloudWatch will add the ingestion time.
+        .without_time()
+        .init();
+
+    run(service_fn(handler)).await
+}
+
+#[tokio::test]
+async fn test_my_lambda_handler() {
+    let input = serde_json::from_str("{\"command\": \"Say Hi!\"}").expect("failed to parse event");
+    let context = lambda_runtime::Context::default();
+
+    let event = lambda_runtime::LambdaEvent::new(input, context);
+
+    handler(event).await.expect("failed to handle event");
 }
